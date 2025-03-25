a4388c1628db1d1ab519997cb90995b9da38f237
ethansmadjaa <ethansmadja@icloud.com>
2025-03-25 09:43:41 +0100

    fix un peu les commentaires

--- parsingsmadjurb/src/main/scala/parser/CsvParser.scala
+++ parsingsmadjurb/src/main/scala/parser/CsvParser.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object CsvParser {
   def readLines(resourceName: String): List[String] = {
     val source = Source.fromResource(resourceName)
-    val lines = source.getLines().toList.drop(1) // on retire l'en-tête
+    val lines = source.getLines().toList.drop(1) // skip header row
     source.close()
     lines
   }
