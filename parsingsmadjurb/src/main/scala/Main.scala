04bdd1152f6d1d20374eb56755cca66dffe257cf
tomGIT <tom.urban@edu.ece.fr>
2025-04-05 13:43:35 +0200

    comments on main

--- parsingsmadjurb/src/main/scala/Main.scala
+++ parsingsmadjurb/src/main/scala/Main.scala
@@ -4,16 +4,17 @@ import ui.ConsoleUI
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // read csv data
+    // Read lines from the CSV files
     val countriesLines = CsvParser.readLines("countries.csv")
     val airportsLines  = CsvParser.readLines("airports.csv")
     val runwaysLines   = CsvParser.readLines("runways.csv")
 
+    // Convert CSV lines into objects (Country, Airport, Runway)
     val countries = countriesLines.flatMap(Country.fromCsv)
     val airports  = airportsLines.flatMap(Airport.fromCsv)
     val runways   = runwaysLines.flatMap(Runway.fromCsv)
 
-    // start the ui menu
+    // Start the user interface (menu in the console)
     ConsoleUI.run(countries, airports, runways)
   }
 }
